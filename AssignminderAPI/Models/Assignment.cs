9b8b7ebbbe4335521cbec481c1ffebc24ff00144
salimelewa <elewas@sheridancollege.ca>
2019-11-17 09:45:21 -0500

    * .gitignore: Intial Commit that includes Assignment class.
    * AssignminderAPI.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * Assignment.cs:
    * AssignminderAPI.csproj:
    * AssignmentContext.cs:
    * appsettings.Development.json:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ AssignminderAPI/Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AssignminderAPI.Models
+{
+    public class Assignment
+    {
+        public string Name { get; set; }
+        public string CourseID { get; set; }
+        public float Grade { get; set; }
+        public DateTimeOffset dueDate { get; set; }
+        public DateTimeOffset Reminder { get; set; }
+
+        public Assignment()
+        {
+        }
+    }
+}
---
+++ AssignminderAPI/Models/AssignmentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AssignminderAPI.Models
+{
+    public class AssignmentContext : DbContext
+    {
+        public AssignmentContext(DbContextOptions<AssignmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment> TodoItems { get; set; }
+    }
+}
