6a05c1eea00bb16c8ce6807db87724a9c36616ab
Salim Elewa <Salim Elewa@DESKTOP-GO42613>
2019-12-12 21:31:41 -0500

    Added Courses controller.

---
+++ AssignminderAPI/Controllers/CourseController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AssignminderAPI.Models;
+
+namespace AssignminderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly CourseContext _context;
+
+        public CoursesController(CourseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        // GET: api/Courses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourse(string id)
+        {
+            var course = await _context.Courses.Where(x => x.UserID == id).ToListAsync();
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/Courses/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(string id, Course course)
+        {
+            if (id != course.Name)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Courses
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostCourse(IEnumerable<Course> courses)
+        {
+            foreach (var item in courses)
+            {
+                _context.Courses.Add(item);
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    if (CourseExists(item.Name))
+                    {
+                        return Conflict();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return CreatedAtAction("GetCourse", courses);
+        }
+
+        // DELETE: api/Courses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Course>> DeleteCourse(string id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return course;
+        }
+
+        private bool CourseExists(string id)
+        {
+            return _context.Courses.Any(e => e.Name == id);
+        }
+    }
+}
